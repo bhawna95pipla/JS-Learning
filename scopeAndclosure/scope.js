0c19157ccf704f33813fd0568314cf780d8f2abb
bhawna95pipla <bhawna95pipla@gmail.com>
2026-09-08 12:42:33 +0530

    scope changes

--- scopeAndclosure/scope.js
+++ scopeAndclosure/scope.js
@@ -19,7 +19,7 @@ inside();
 //console.log(fridge);                            // fridge is not defined
 
 
-// 3. Block Scope  : anu curly braces with if , for is a block 
+// 3. Block Scope  : any curly braces with if , for is a block 
 
 if (true){
     let safeVault = "Gold access Granted"
@@ -28,22 +28,31 @@ if (true){
 //console.log(safeVault);                           // safeVault is not defined
 
 
+//Exmaple 2 : Simple curly braces is also a block wihtout any if , for 
+{
+    let x=10;
+    console.log(x);                                 // 10
+}
+console.log(x);                                     // x is not defined
+
+
+
 // 4. Difference between function/local VS Block scope : A block lives inside a function, but a function can not live inside a block.
-// var does not repect block scope but does respect Function/local scopr
+// var does not respect block scope but does respect Function/local scope
 
 function scopes(){
     if (true){
         var varScope = "Can be accessed inside whole function";
-        let letScope = "Limited to this for loop Block";
+        let letScope = "Limited to this if loop Block";
         const constScope = "Also Limited to if loop block";
 
     console.log("Inside if Loop: ", varScope);                 // Inside if Loop:  Can be accessed inside whole function
-    console.log("Inside if Loop: ", letScope);                 // Inside if Loop:  Limited to this for loop Block
+    console.log("Inside if Loop: ", letScope);                 // Inside if Loop:  Limited to this if loop Block
     console.log("Inside if Loop: ", constScope);               // Inside if Loop:  Also Limited to if loop block
     }
     console.log("Outside Loop: ", varScope);                   // Outside Loop:  Can be accessed inside whole function
-    console.log("Outside Loop: ", letScope);                  // letScope is not defined
-    console.log("Outside Loop: ", constScope);                  // constScope is not defined
+    console.log("Outside Loop: ", letScope);                   // letScope is not defined
+    console.log("Outside Loop: ", constScope);                 // constScope is not defined
 };
 
 scopes();
